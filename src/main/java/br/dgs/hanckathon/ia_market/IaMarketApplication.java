f13c4bcfea9360e27ec7c9f5ddca819c03da8a6b
hendrew.martins <hendrewmartins@hotmail.com>
2025-09-18 15:33:38 -0300

    init

---
+++ src/main/java/br/dgs/hanckathon/ia_market/IaMarketApplication.java
@@ -0,0 +1,13 @@
+package br.dgs.hanckathon.ia_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dgs/hanckathon/ia_market/IaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dgs.hanckathon.ia_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
